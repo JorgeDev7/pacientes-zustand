770c9de71c333d95721c326fcdbedcd63ab7b985
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-04-22 10:25:49 -0600

    Edit and Delete button added

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -34,6 +34,17 @@ export default function PatientDetails({ patient }: PatientDetailsProps) {
                 label="Síntomas"
                 data={symptoms}
             />
+
+            <div className="flex flex-col md:flex-row justify-between gap-3 mt-10">
+                <button
+                    type="button"
+                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+                >Editar</button>
+                <button
+                    type="button"
+                    className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+                >Eliminar</button>
+            </div>
         </div>
     );
 }
