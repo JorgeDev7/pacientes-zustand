4debd0db655b3b5d49a3d6600560eb7457d7941e
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-04-22 11:25:42 -0600

    Adding ToastContainer and Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import { ToastContainer } from "react-toastify";
+
 import PatientForm from "./components/PatientForm";
 import PatientList from "./components/PatientList";
+
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
@@ -13,6 +17,8 @@ function App() {
           <PatientList />
         </div>
       </div>
+
+      <ToastContainer />
     </>
   );
 }
