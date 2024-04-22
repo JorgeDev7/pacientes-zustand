af37288c23fc2aa1079e512d1990c8aa943801fa
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-04-22 11:11:41 -0600

    Set actions to each buttons

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,4 +1,5 @@
 import PatientDetailItem from "./PatientDetailItem";
+import { usePatientStore } from "../store";
 
 type PatientDetailsProps = {
     patient: Patient;
@@ -7,6 +8,9 @@ type PatientDetailsProps = {
 export default function PatientDetails({ patient }: PatientDetailsProps) {
 
     const { id, name, caretaker, email, date, symptoms } = patient;
+
+    const deletePatient = usePatientStore((state) => state.deletePatient);
+    const getPatientById = usePatientStore((state) => state.getPatientById);
 
     return (
         <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
@@ -35,14 +39,16 @@ export default function PatientDetails({ patient }: PatientDetailsProps) {
                 data={symptoms}
             />
 
-            <div className="flex flex-col md:flex-row justify-between gap-3 mt-10">
+            <div className="flex flex-col lg:flex-row justify-between gap-3 mt-10">
                 <button
                     type="button"
                     className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+                    onClick={() => getPatientById(id)}
                 >Editar</button>
                 <button
                     type="button"
                     className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+                    onClick={() => deletePatient(id)}
                 >Eliminar</button>
             </div>
         </div>
